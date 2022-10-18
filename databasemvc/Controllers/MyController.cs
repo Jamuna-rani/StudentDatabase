e8786306186c80bd2dc8440c5656e644c904578d
STS664-JAMUNARANI GK <STS664-JAMUNARANI GK@STS664L-JAMUNAR>
2022-10-18 16:56:20 +0530

    Add project files.

---
+++ databasemvc/Controllers/MyController.cs
@@ -0,0 +1,39 @@
+﻿using databasemvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace databasemvc.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            return View();
+        }
+       public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult Mydetail(detail information)
+        {
+            using(var table=new jamunaEntities() )
+            {
+                mytable data = new mytable();
+                data.Roll_No = information.RollNo;
+                data.Student_Name = information.StudentName;
+                data.Student_Age=information.StudentAge;
+                data.Student_Location = information.StudentLocation;
+                data.Phone_No=information.PhoneNo;
+                table.mytables.Add(data);
+                table.SaveChanges();
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ databasemvc/Models/detail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace databasemvc.Models
+{
+    public class detail
+    {
+        public int RollNo { get; set; }
+        public string StudentName { get; set; }
+        public int StudentAge { get; set; }
+        public string StudentLocation { get; set; }
+        public long PhoneNo { get; set; }
+    }
+}
